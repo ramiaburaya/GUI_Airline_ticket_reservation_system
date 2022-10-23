60243886c90411121b28c1fba406f652fefde618
Aburayya <ramiaburaya16@gmail.com>
2022-10-23 16:42:10 +0300

    Create Flight class with its method

---
+++ src/main/java/com/example/gui_airline_ticket_reservation_system/Flight.java
@@ -0,0 +1,77 @@
+package com.example.gui_airline_ticket_reservation_system;
+
+public class Flight implements Comparable<Flight> {
+    private int flightNumber;
+    private String airLineName;
+    private String source;
+    private String destination;
+    private int capacity;
+
+    //parametrized Constructor
+    public Flight(int flightNumber, String airLineName, String source, String destination, int capacity) {
+        this.flightNumber = flightNumber;
+        this.airLineName = airLineName;
+        this.source = source;
+        this.destination = destination;
+        this.capacity = capacity;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getAirLineName() {
+        return airLineName;
+    }
+
+    public void setAirLineName(String airLineName) {
+        this.airLineName = airLineName;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    // we Override equals Method because we will search by Flight number
+    @Override
+    public boolean equals(Object o) {
+        return this.getFlightNumber() == ((Flight) o).getFlightNumber();
+    }
+
+    // we Override compareTo Method because we will insert Flight in linked list Sorted
+    @Override
+    public int compareTo(Flight o) {
+        if (this.getFlightNumber() == o.getFlightNumber()) {
+            return 0;
+        } else if (this.getFlightNumber() > o.getFlightNumber()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+}
