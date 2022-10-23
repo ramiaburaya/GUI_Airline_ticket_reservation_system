fad49d78cc737215c767751d8ef974012cbb269c
Aburayya <ramiaburaya16@gmail.com>
2022-10-23 19:10:57 +0300

    Create the Read GUI

--- src/main/java/com/example/gui_airline_ticket_reservation_system/LoginWindow.java
+++ src/main/java/com/example/gui_airline_ticket_reservation_system/LoginWindow.java
@@ -4,6 +4,7 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -13,7 +14,8 @@ import javafx.stage.Stage;
 public class LoginWindow {
     private final String username_admin = "rami";
     private final String password_admin = "rami2006";
-    public String ButtonColor = "-fx-background-color: #98A8F8";
+    public static String ButtonColor = "-fx-background-color: #98A8F8";
+    public static Image icon = new Image("file:C:\\Users\\rami0\\Desktop\\Java_Project\\GUI_Airline_ticket_reservation_system\\src\\main\\java\\com\\example\\gui_airline_ticket_reservation_system\\imgs\\airplane.png");
 
     public void printLoginWindow() {
         Stage mainStage = new Stage();
@@ -22,6 +24,14 @@ public class LoginWindow {
         AnchorPane rootPane = new AnchorPane();
         rootPane.setPrefSize(600, 400);
         rootPane.setBackground(new Background(new BackgroundFill(Color.rgb(205, 252, 246), null, null)));
+
+        //image
+        ImageView image = new ImageView();
+        image.setImage(new Image("file:C:\\Users\\rami0\\Desktop\\Java_Project\\GUI_Airline_ticket_reservation_system\\src\\main\\java\\com\\example\\gui_airline_ticket_reservation_system\\imgs\\Login.png"));
+        image.setFitHeight(122);
+        image.setFitWidth(131);
+        image.setLayoutX(14);
+        image.setLayoutY(14);
 
         //labels
         Label userName = new Label("User name");
@@ -56,7 +66,9 @@ public class LoginWindow {
         login.setCursor(Cursor.HAND);
         login.setOnAction(e -> {
             if (UserNameTxt.getText().equals(username_admin) && passwordTxt.getText().equals(password_admin)) {
-                //toDo
+                ReadWindow readWindow = new ReadWindow();
+                readWindow.ReadWin();
+                mainStage.close();
             } else {
                 Alert Error = new Alert(Alert.AlertType.ERROR);
                 Error.setTitle("Wrong");
@@ -70,19 +82,17 @@ public class LoginWindow {
         Exit.setLayoutX(516);
         Exit.setLayoutY(356);
         Exit.setStyle(ButtonColor);
-        Exit.setCursor(Cursor.HAND);
-        Exit.setOnAction(e ->
-                mainStage.close()
-        );
+        Exit.setCursor(Cursor.CLOSED_HAND);
+        Exit.setOnAction(e -> mainStage.close());
 
-        rootPane.getChildren().addAll(userName, password, UserNameTxt, passwordTxt, login, Exit);
+        rootPane.getChildren().addAll(userName, password, UserNameTxt, passwordTxt, login, Exit, image);
 
         //Scene
         Scene mainScene = new Scene(rootPane);
         mainStage.setScene(mainScene);
-        mainStage.setTitle("Login ");
+        mainStage.setTitle("Read Files ");
         //icon for login page
-        mainStage.getIcons().add(new Image("file:C:\\Users\\rami0\\Desktop\\Java_Project\\GUI_Airline_ticket_reservation_system\\src\\main\\java\\com\\example\\gui_airline_ticket_reservation_system\\imgs\\Login.png"));
+        mainStage.getIcons().add(icon);
         mainStage.show();
     }
 }
---
+++ src/main/java/com/example/gui_airline_ticket_reservation_system/ReadWindow.java
@@ -0,0 +1,70 @@
+package com.example.gui_airline_ticket_reservation_system;
+
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class ReadWindow {
+    public void ReadWin() {
+        Stage mainStage = new Stage();
+
+        AnchorPane rootPane = new AnchorPane();
+        rootPane.setPrefSize(600, 400);
+        rootPane.setBackground(new Background(new BackgroundFill(Color.rgb(205, 252, 246), null, null)));
+
+        //image
+        ImageView image = new ImageView();
+        image.setImage(new Image("file:C:\\Users\\rami0\\Desktop\\Java_Project\\GUI_Airline_ticket_reservation_system\\src\\main\\java\\com\\example\\gui_airline_ticket_reservation_system\\imgs\\ReadFiles.png"));
+        image.setFitHeight(122);
+        image.setFitWidth(131);
+        image.setLayoutX(14);
+        image.setLayoutY(14);
+
+        //Buttons
+        Button ReadFlights = new Button("Read Flights");
+        ReadFlights.setPrefSize(150, 30);
+        ReadFlights.setLayoutX(224);
+        ReadFlights.setLayoutY(153);
+        ReadFlights.setStyle(LoginWindow.ButtonColor);
+        ReadFlights.setCursor(Cursor.HAND);
+
+        Button ReadPassenger = new Button("Read Passengers");
+        ReadPassenger.setPrefSize(150, 30);
+        ReadPassenger.setLayoutX(224);
+        ReadPassenger.setLayoutY(216);
+        ReadPassenger.setStyle(LoginWindow.ButtonColor);
+        ReadPassenger.setCursor(Cursor.HAND);
+
+
+        Button LogOut = new Button("LogOut");
+        LogOut.setPrefSize(70, 30);
+        LogOut.setLayoutX(516);
+        LogOut.setLayoutY(356);
+        LogOut.setStyle(LoginWindow.ButtonColor);
+        LogOut.setCursor(Cursor.CLOSED_HAND);
+        LogOut.setOnAction(e -> {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.printLoginWindow();
+            mainStage.close();
+        });
+
+        rootPane.getChildren().addAll(ReadPassenger, ReadFlights, LogOut, image);
+
+        //Scene
+        Scene mainScene = new Scene(rootPane);
+        mainStage.setScene(mainScene);
+        mainStage.setTitle("Login ");
+
+        //icon for login page
+        mainStage.getIcons().add(LoginWindow.icon);
+        mainStage.show();
+    }
+}
