99167cca6272e722eaa5005fe5ce4e202895d149
Aburayya <ramiaburaya16@gmail.com>
2022-10-23 03:50:33 +0300

    Create the structure of Project

---
+++ src/main/java/com/example/gui_airline_ticket_reservation_system/HelloApplication.java
@@ -0,0 +1,16 @@
+package com.example.gui_airline_ticket_reservation_system;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        //here where the program should start
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
