a4b6fbd4f60e7b620aab0c6d2b0eb2b5bc2e52fb
Aburayya <ramiaburaya16@gmail.com>
2022-10-25 15:27:33 +0300

    No changes

--- src/main/java/com/example/gui_airline_ticket_reservation_system/HelloApplication.java
+++ src/main/java/com/example/gui_airline_ticket_reservation_system/HelloApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
-
     @Override
     public void start(Stage stage) {
         LoginWindow s = new LoginWindow();
